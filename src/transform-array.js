a4d8aec4a44d254dcdceff12db4765d84e654331
Sergey P <s.pansevi4@gmail.com>
2022-10-13 23:40:28 +0300

    feat: conplete next three task

--- src/transform-array.js
+++ src/transform-array.js
@@ -3,21 +3,82 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+	const stack = [];
+	const result = [];
+
+	if (!Array.isArray(arr)) {
+		throw Error(`'arr' parameter must be an instance of the Array!`);
+	}
+
+	if (arr.length === 0) {
+		return arr;
+	}
+
+	arr.forEach((el, i) => {
+		if (stack.length !== 0) {
+			result.push(arr[i]);
+			result.pop();
+			stack.pop();
+			return;
+		}
+
+		if ((typeof el === 'string')) {
+			const operation = el;
+
+			switch (operation) {
+				case '--double-next':
+					if (i + 1 >= arr.length) {
+						return result;
+					}
+					result.push(arr[i + 1]);
+					break;
+
+				case '--double-prev':
+					if (i - 1 < 0) {
+						break;
+					}
+					if (result[result.length - 1] === arr[i - 1]) {
+						result.push(arr[i - 1]);
+					}
+					break;
+
+				case '--discard-next':
+					if (i + 1 >= arr.length) {
+						return result;
+					}
+					stack.push(el);
+					break;
+
+				case '--discard-prev':
+					if (i - 1 < 0) {
+						break;
+					}
+					if (result[result.length - 1] === arr[i - 1]) {
+						result.pop();
+					}
+					break;
+
+				default: result.push(el);
+			}
+		} else {
+			result.push(el);
+		}
+	});
+
+	return result;
 }
 
 module.exports = {
-  transform
+	transform
 };
--- src/what-season.js
+++ src/what-season.js
@@ -2,20 +2,51 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+	try {
+		const season = [
+			[11, 0, 1],
+			[2, 3, 4],
+			[5, 6, 7],
+			[8, 9, 10]];
+
+		const month = [
+			'winter',
+			'spring',
+			'summer',
+			'autumn'];
+
+		if (date === undefined) {
+			return 'Unable to determine the time of year!';
+		}
+
+		if (isNaN(date.getTime())) {
+			Error('Invalid date!');
+		}
+
+		const monthNumber = date.getMonth();
+
+		for (let i = 0; i < season.length; i++) {
+			for (let j = 0; j < season[0].length; j++) {
+				if (season[i][j] === monthNumber) {
+					return month[i];
+				}
+			}
+		}
+	} catch {
+		throw Error('Invalid date!');
+	}
 }
 
 module.exports = {
-  getSeason
+	getSeason
 };
